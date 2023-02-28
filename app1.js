e42d5925284a05ec62afa6e320e2eeaefe21eb7c
EZZINE <anass.ezzine@etu.univ-amu.fr>
2023-02-28 15:04:18 +0100

    fonction getCounterCompany & getCounterCountry

--- app1.js
+++ app1.js
@@ -6,20 +6,36 @@ let users = JSON.parse(rawdata);
 
 // Récupérer l'argument d'entrée
 // indice est la position de l'élèment qu'on veut récuperer 
-function getArg(indice)
+function getArg()
 {
-  const input = process.argv[indice];
+  const input = process.argv[2];
   return input;
 }
 
-
-if(input === 'country')
+// Calculer le compteur d'utilisateurs par pays
+function getCounterCountry()
 {
-    // Calculer le compteur d'utilisateurs par pays
   const countByCountry = users.reduce((acc, user) => {
-    acc[user.country] = (acc[user.country] || 0) + 1;
+    acc[user.country] = (acc[user.country] ?? 0) + 1;
     return acc;
   }, {});
+  return countByCountry;
+}
+
+// Calculer le compteur d'utilisateurs par company
+function getCounterCompany()
+{
+  const countByComapny = users.reduce((acc, user) => {
+    acc[user.company] = (acc[user.company] ?? 0) + 1;
+    return acc;
+  }, {});
+  return countByComapny;
+}
+
+if(getArg(2) === 'country')
+{
+    // Calculer le compteur d'utilisateurs par pays
+  countByCountry = getCounterCountry()
 
   // Trier les pays par ordre décroissant de compteur d'utilisateurs
   const sortedByCount = Object.entries(countByCountry).sort((a, b) => b[1] - a[1]);
@@ -30,13 +46,10 @@ if(input === 'country')
   });
 }
 
-else if(input === 'company')
+else if(getArg(2) === 'company')
 {
     // Calculer le compteur d'utilisateurs par company
-  const countByCompany = users.reduce((acc, user) => {
-    acc[user.company] = (acc[user.company] || 0) + 1;
-    return acc;
-  }, {});
+  const countByCompany = getCounterCompany()
 
   // Trier les companies par ordre décroissant de compteur d'utilisateurs
   const sortedByCount = Object.entries(countByCompany).sort((a, b) => b[1] - a[1]);
