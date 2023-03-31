15c9d41a45114790716106c96913679f5ad8871a
EZZINE <anass.ezzine@etu.univ-amu.fr>
2023-03-31 08:43:00 +0200

    modification sur business.js

--- business/business.js
+++ business/business.js
@@ -1,38 +1,65 @@
 const dal = require("../data/datalayer");
 // const _ = require("underscore"); //voir a quoi ca sert
  
+
 const defaultNumber = 10;
 const defaultPage = 1;
 const maxNumber = 100;
 
 const business = {
-    getAllCustomers : function() {
+    getAllCustomers: function () {
         return dal.getAllCustomers();
     },
 
-    getCustomers : function(number, page) {
-        //checks param
-        if(number === undefined || page === undefined){
+    getCustomers: function (number, page) {
+        // Vérifier les paramètres
+        if (number === undefined || page === undefined) {
             number = defaultNumber;
             page = defaultPage;
         }
-        if(number > maxNumber){
+        if (number > maxNumber) {
             number = maxNumber;
         }
 
-        //recupérer données de la DAL
+        // Récupérer les données de la DAL
         const resCustomers = dal.getCustomers(number, page);
 
-        resCustomers.page = page;
-        resCustomers.numberByPage = number;
-        resCustomers.totalPages = Math.ceil(resCustomers.total / number);
+        // Ajouter des informations supplémentaires
+        const totalPages = Math.ceil(resCustomers.total / number);
+        const hasNextPage = page < totalPages;
+        const hasPreviousPage = page > 1;
 
-        //return customers
-        return resCustomers;
-    }
+        // Construire l'objet réponse
+        const response = {
+            customers: resCustomers.result,
+            pagination: {
+                numberByPage: number,
+                page: page,
+                total: resCustomers.total,
+                totalPages: totalPages,
+                hasNextPage: hasNextPage,
+                hasPreviousPage: hasPreviousPage,
+            },
+        };
 
-    
+        // Retourner la réponse
+        return response;
+    },
 
+    addCustomer: function (customer) {
+        // TODO: Implémenter la logique pour ajouter un client
+        throw new Error("Not implemented yet");
+    },
+
+    updateCustomer: function (id, customer) {
+        // TODO: Implémenter la logique pour mettre à jour un client
+        throw new Error("Not implemented yet");
+    },
+
+    deleteCustomer: function (id) {
+        // TODO: Implémenter la logique pour supprimer un client
+        throw new Error("Not implemented yet");
+    },
 };
 
 module.exports = business;
